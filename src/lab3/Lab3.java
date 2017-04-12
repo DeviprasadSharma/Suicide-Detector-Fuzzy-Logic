c45a2ca98cb8b9e0803e28add8db5b959bfb4f66
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2017-04-11 23:54:18 -0300

    ADD: project

---
+++ src/lab3/Lab3.java
@@ -0,0 +1,17 @@
+
+
+package lab3;
+
+import nrc.fuzzy.*;
+
+public class Lab3 {
+
+    public static void main(String[] args) {
+               
+        Interfaz Ventana = new Interfaz();
+        Ventana.setVisible(true); 
+        
+        
+    }
+    
+}
